432e1ae81b942c9b530616b926e4d8bdffef7a64
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-16 09:17:22 +0530

    Update init.py

--- bot/handlers/init.py
+++ bot/handlers/init.py
@@ -4,6 +4,7 @@ from .search import setup as search_handlers
 from .user import setup as user_handlers
 
 def register_handlers(application):
+    """Register all handlers with the application"""
     user_handlers(application)
     search_handlers(application)
     download_handlers(application)
